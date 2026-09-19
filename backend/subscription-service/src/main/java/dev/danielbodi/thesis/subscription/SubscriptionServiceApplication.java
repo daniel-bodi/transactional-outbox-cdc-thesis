2eca7d911bf7f3d52de42a61a6870be28490200e
danielbodi <bodi.daniel@hibridlevel.hu>
2026-09-19 12:08:51 +0200

    refactor(backend): cleaner naming and versioning

--- backend/subscription-service/src/main/java/dev/danielbodi/thesis/subscription/SubscriptionServiceApplication.java
+++ backend/subscription-service/src/main/java/dev/danielbodi/thesis/subscription/SubscriptionServiceApplication.java
@@ -1,4 +1,4 @@
-package dev.danielbodi.subscription;
+package dev.danielbodi.thesis.subscription;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
