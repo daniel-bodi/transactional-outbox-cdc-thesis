9c07393b576e397febf18f5b7e186bb07d8bc6c2
danielbodi <bodi.daniel@hibridlevel.hu>
2026-09-19 11:50:14 +0200

    feat(backend): add parent and empty microservice

---
+++ backend/subscription-service/src/main/java/dev/danielbodi/subscription/SubscriptionApplication.java
@@ -0,0 +1,14 @@
+package dev.danielbodi.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author danielbodi
+ */
+@SpringBootApplication
+public class SubscriptionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionApplication.class, args);
+    }
+}
