d65bea924c0b9c4a531cbee7571cbc8bc7b6086c
danielbodi <bodi.daniel@hibridlevel.hu>
2026-09-21 11:17:05 +0200

    feat(backend): add empty payment-service module

---
+++ backend/payment-service/src/test/java/dev/danielbodi/thesis/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danielbodi.thesis.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
